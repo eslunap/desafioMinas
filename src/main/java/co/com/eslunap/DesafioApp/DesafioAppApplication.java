532422a9790d0d172fb556f2943a19cea006face
eslunap19 <estefany.luna@gmail.com>
2021-01-20 02:01:11 -0500

    Commit Setup. Close #1

---
+++ src/main/java/co/com/eslunap/DesafioApp/DesafioAppApplication.java
@@ -0,0 +1,13 @@
+package co.com.eslunap.DesafioApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/eslunap/DesafioApp/DesafioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.eslunap.DesafioApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
